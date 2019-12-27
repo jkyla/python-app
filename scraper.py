8b06b9d6d0c3f71fd630c8d1b9075347a8402144
Kyla Johnson <johnsonkyla18@yahoo.com>
2019-12-27 12:37:08 -0600

    import

---
+++ scraper.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://www.amazon.com/Sony-Mirrorless-Digital-Camera-28-70mm/dp/B00PX8CNCM/ref=sr_1_4?keywords=sony+a7&qid=1577471091&sr=8-4'
+
+headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"}
+
+page = requests.get(URL, headers=headers)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+title = soup.find(id="productTitle").get_text()
+
+print(title.strip())
